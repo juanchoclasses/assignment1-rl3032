79ecedfdd38d53ce8636a40ed9a83d04dfed858c
rl3032 <runjielyu@gmail.com>
2023-10-02 01:35:55 -0700

    Rewrite the evaluate and add related private methods

--- src/Engine/FormulaEvaluator.ts
+++ src/Engine/FormulaEvaluator.ts
@@ -1,93 +1,135 @@
-import Cell from "./Cell"
-import SheetMemory from "./SheetMemory"
+import Cell from "./Cell";
+import SheetMemory from "./SheetMemory";
 import { ErrorMessages } from "./GlobalDefinitions";
 
-
+export type FormulaType = string[];
+export type TokenType = string;
 
 export class FormulaEvaluator {
-  // Define a function called update that takes a string parameter and returns a number
-  private _errorOccured: boolean = false;
+  private _errorOccurred: boolean = false;
   private _errorMessage: string = "";
   private _currentFormula: FormulaType = [];
-  private _lastResult: number = 0;
+  private _currentTokenIndex: number = 0;
   private _sheetMemory: SheetMemory;
   private _result: number = 0;
-
+  private _lastResult: number = 0; // Initialized
 
   constructor(memory: SheetMemory) {
-    this._sheetMemory = memory;
+      this._sheetMemory = memory;
   }
 
-  /**
-    * place holder for the evaluator.   I am not sure what the type of the formula is yet 
-    * I do know that there will be a list of tokens so i will return the length of the array
-    * 
-    * I also need to test the error display in the front end so i will set the error message to
-    * the error messages found In GlobalDefinitions.ts
-    * 
-    * according to this formula.
-    * 
-    7 tokens partial: "#ERR",
-    8 tokens divideByZero: "#DIV/0!",
-    9 tokens invalidCell: "#REF!",
-  10 tokens invalidFormula: "#ERR",
-  11 tokens invalidNumber: "#ERR",
-  12 tokens invalidOperator: "#ERR",
-  13 missingParentheses: "#ERR",
-  0 tokens emptyFormula: "#EMPTY!",
+  private get currentToken(): TokenType {
+      return this._currentFormula[this._currentTokenIndex];
+  }
 
-                    When i get back from my quest to save the world from the evil thing i will fix.
-                      (if you are in a hurry you can fix it yourself)
-                               Sincerely 
-                               Bilbo
-    * 
-   */
+  private advanceToken(): void {
+      this._currentTokenIndex++;
+  }
 
-  evaluate(formula: FormulaType) {
+  private matchToken(expected: TokenType): boolean {
+      if (this.currentToken === expected) {
+          this.advanceToken();
+          return true;
+      } else {
+          this._lastResult = NaN; // Updated
+          this._errorMessage = ErrorMessages.invalidFormula;
+          return false;
+      }
+  }
 
+  private factor(): number {
+      if (this.isNumber(this.currentToken)) {
+          const value = Number(this.currentToken);
+          this.advanceToken();
+          return value;
+      } else if (this.isCellReference(this.currentToken)) {
+          const [value, error] = this.getCellValue(this.currentToken);
+          if (error) {
+              this._lastResult = NaN; // Updated
+              this._errorMessage = error;
+              this._errorOccurred = true;
+          }
+          this.advanceToken();
+          return value;
+      } else if (this.currentToken === '(') {
+          this.advanceToken();
+          const value = this.expression();
+          this.matchToken(')');
+          return value;
+      } else {
+          this._lastResult = NaN; // Updated
+          this._errorMessage = ErrorMessages.invalidFormula;
+          this._errorOccurred = true;
+          return 0;
+      }
+  }
 
-    // set the this._result to the length of the formula
-
-    this._result = formula.length;
-    this._errorMessage = "";
-
-    switch (formula.length) {
-      case 0:
-        this._errorMessage = ErrorMessages.emptyFormula;
-        break;
-      case 7:
-        this._errorMessage = ErrorMessages.partial;
-        break;
-      case 8:
-        this._errorMessage = ErrorMessages.divideByZero;
-        break;
-      case 9:
-        this._errorMessage = ErrorMessages.invalidCell;
-        break;
-      case 10:
-        this._errorMessage = ErrorMessages.invalidFormula;
-        break;
-      case 11:
-        this._errorMessage = ErrorMessages.invalidNumber;
-        break;
-      case 12:
-        this._errorMessage = ErrorMessages.invalidOperator;
-        break;
-      case 13:
-        this._errorMessage = ErrorMessages.missingParentheses;
-        break;
-      default:
-        this._errorMessage = "";
-        break;
+  private term(): number {
+    let value = this.factor();
+    while (this.currentToken === '*' || this.currentToken === '/') {
+        const op = this.currentToken;
+        this.advanceToken();
+        if (op === '*') {
+            value *= this.factor();
+        } else if (op === '/') {
+            const divisor = this.factor();
+            if (divisor === 0) {
+                this._errorMessage = ErrorMessages.divideByZero; 
+                this._errorOccurred = true;
+                this._lastResult = Infinity;
+                return Infinity;
+            }
+            value /= divisor;
+        }
     }
+    return value;
+  }
+
+  private expression(): number {
+      let value = this.term();
+      while (this.currentToken === '+' || this.currentToken === '-') {
+          const op = this.currentToken;
+          this.advanceToken();
+          if (op === '+') {
+              value += this.term();
+          } else if (op === '-') {
+              value -= this.term();
+          }
+      }
+      return value;
+  }
+
+  public evaluate(formula: FormulaType): number {
+      this._currentFormula = formula;
+      this._currentTokenIndex = 0;
+      this._errorOccurred = false;
+      this._errorMessage = "";
+      
+      if (formula.length === 0) {
+          this._errorMessage = ErrorMessages.emptyFormula;
+          return 0;
+      }
+
+      this._result = this.expression();
+      this._lastResult = this._result; // Updated
+      if (this._currentTokenIndex !== formula.length && !this._errorOccurred) {
+        this._errorMessage = ErrorMessages.invalidFormula;
+        return 0;
+      }
+    
+      return this._result;
   }
 
   public get error(): string {
-    return this._errorMessage
+      return this._errorMessage;
   }
 
   public get result(): number {
-    return this._result;
+      return this._result;
+  }
+
+  public get lastResult(): number { // Added getter for lastResult
+      return this._lastResult;
   }
 
 
